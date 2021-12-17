1b2160cedbe70058e85f43bce6b60ac60a47b553
gksmf0324 <gksmfdl0324@naver.com>
2021-12-17 10:33:59 +0900

    Day09

--- 비교/Day09/js/02_full_dropdown.js
+++ 비교/Day09/js/02_full_dropdown.js
@@ -5,23 +5,22 @@
 
 $(function() {
 
-    //메인 메뉴 - 마우스 올렸을 때 이벤트
+    // 메인 메뉴 - 마우스 올렸을 때 이벤트
     // $('nav > ul > li').on('mouseover', function() {
     //     $(this).children('.submenu').stop().slideDown(1000)
     // })
 
-    //메인 메뉴 - 마우스 벗어났을 때 이벤트
-    // $('nav > ul > li').on('mouseout', function(){
+    // // 메인 메뉴 - 마우스가 벗어날 때 이벤트
+    // $('nav > ul > li').on('mouseout', function() {
     //     $(this).children('.submenu').stop().slideUp(1000)
     // })
 
-
-    //메인 메뉴 - 마우스 클릭했을 때 이벤트
+    // 메인 메뉴 - 마우스 클릭 이벤트
     $('nav > ul > li').on('click', function() {
-        //현재 선택한 메인 메뉴 아래 서브메뉴만 슬라이드
+        // 현재 선택한 메인 메뉴 아래 서브 메뉴만 슬라이드
         // $(this).children('.submenu').stop().slideToggle(1000)
-        
-        //모든 서브메뉴 슬라이드
+
+        // 모든 서브 메뉴 슬라이드
         $('.submenu').stop().slideToggle(1000)
     })
 
